4d3e1a1a178deddbe872bee1a15052e57ed44e20
moodylife <36269891+moodylife@users.noreply.github.com>
2018-12-21 15:38:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
- client.user.setGame('Txe'Security.','https://www.twitch.tv/peery13');
+ client.user.setGame('Txe|Security.','https://www.twitch.tv/peery13');
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
